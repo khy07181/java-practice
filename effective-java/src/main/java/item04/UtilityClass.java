458adc2624e54af565272a036b6da92d15ddf1a9
khy07181 <khy07181@gmail.com>
2024-01-24 00:08:25 +0900

    Effective-Java: 인스턴스화를 막으려거든 private 생성자를 사용하라.

---
+++ effective-java/src/main/java/item04/UtilityClass.java
@@ -0,0 +1,20 @@
+package item04;
+
+public class UtilityClass {
+
+    private UtilityClass() {
+        throw new AssertionError();
+    }
+
+    public static String hello() {
+        return "hello";
+    }
+
+    public static void main(String[] args) {
+        String hello = UtilityClass.hello();
+
+        UtilityClass utilityClass = new UtilityClass();
+        utilityClass.hello();
+    }
+
+}
