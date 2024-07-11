157f4cf9d0970f2a49c1f688a635d0b03991c02e
hayoung <khy07181@gmail.com>
2024-07-11 14:06:56 +0900

    .

--- effective-java/src/test/java/item05/staticutils/SpellCheckerTest.java
+++ effective-java/src/test/java/item05/staticutils/SpellCheckerTest.java
@@ -2,13 +2,14 @@ package item05.staticutils;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SpellCheckerTest {
 
     @Test
     void isValid() {
-        assertTrue(SpellChecker.isValid("test"));
+        assertFalse(SpellChecker.isValid("test"));
     }
 
 }
