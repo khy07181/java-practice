c16c14297d97bd81ca42ca4f5ffa1e82e11723ee
khy07181 <khy07181@gmail.com>
2024-08-06 23:33:14 +0900

    design-patterns: Facade

---
+++ design-pattern/src/main/java/creational_patterns/facade/file/FileWriter.java
@@ -0,0 +1,11 @@
+package creational_patterns.facade.file;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FileWriter {
+    public void writeFile(String filePath, String content) throws IOException {
+        Files.write(Paths.get(filePath), content.getBytes());
+    }
+}
---
+++ design-pattern/src/main/java/creational_patterns/facade/smart_home/CoffeeMaker.java
@@ -0,0 +1,7 @@
+package creational_patterns.facade.smart_home;
+
+public class CoffeeMaker {
+    public void brewCoffee() {
+        System.out.println("Brewing coffee.");
+    }
+}
